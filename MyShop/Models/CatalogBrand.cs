58cbc87d96f386febc6e4c5b6ad1da765847ffbc
pavelkarpovich2015 <pavelkarpovich2015@gmail.com>
2022-12-07 18:43:13 +0300

    Add Catalog controller (set as default) + some action methods

---
+++ MyShop/Models/CatalogBrand.cs
@@ -0,0 +1,8 @@
+﻿namespace MyShop.Models
+{
+    public sealed class CatalogBrand
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+    }
+}
---
+++ MyShop/Models/CatalogItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyShop.Models
+{
+    public sealed class CatalogItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
